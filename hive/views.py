5d9bb6ef1e5c62a3e0ff4a893a470d7fd2fac10e
kilonzi <vkilonzi05@gmail.com>
2021-11-24 12:50:41 +0300

    created a view functionality for searching images

--- hive/views.py
+++ hive/views.py
@@ -1,3 +1,4 @@
+from django.core.checks import messages
 from django.shortcuts import render
 from .models import Image
 from django.http import Http404
@@ -7,7 +8,7 @@ from django.core.exceptions import ObjectDoesNotExist
  
 def photos(request):
     """Function for Images views functionality"""
-    
+
     categories = Image.objects.distinct().values_list('category_name', flat=True)
     locations = Image.objects.distinct().values_list('location_name', flat=True)
     try:
@@ -16,3 +17,15 @@ def photos(request):
         raise Http404
     return render(request, photos.html, {'image':images, 'categories':categories, 'locations':locations})
 
+def search_images(request):
+    if 'image' in request.GET and request.GET['image']:
+        search_term = request.GET.get('image')
+        searched_images =Image.search_by_name(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html', {'message': message, 'images':searched_images})
+    else:
+        message = "Kindly Feed in a valid Input!"
+        return render(request, 'search.html', {'message':message})
+
+    
